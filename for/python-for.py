90b220363a4fdfc3a41e11eab6e3c43b20488ba8
cyber <victor.gonzalez.a@gmail.com>
2023-11-14 11:39:28 -0300

    Probando git ssh 3

--- for/python-for.py
+++ for/python-for.py
@@ -6,3 +6,9 @@ data = [(1,2),(3,4),(5,6)]
     
 for x,y in data:
     print(f" {x} e {y}")
+    
+buscar = 5
+for numero in range(10):
+    if numero == buscar:
+        print(f"Encontrado {numero}")
+        break
